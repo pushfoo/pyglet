42a20741db351d9576c8002ab7832e308522bac8
pushfoo <36696816+pushfoo@users.noreply.github.com>
2023-09-08 06:15:15 -0400

    Add top-level shader automock fixtures

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -1,11 +1,13 @@
 import re
-from typing import Tuple, Callable
-from unittest.mock import NonCallableMagicMock
+from typing import Tuple, Callable, Union, Dict, cast
+from unittest.mock import NonCallableMagicMock, Mock
 
 import pytest
 from pytest import fixture
 
 from pyglet import gl
+from pyglet.gl import ObjectSpace
+from pyglet.graphics import Batch, Group
 from pyglet.graphics.shader import ShaderProgram
 from tests.base.mock_helpers import (
     DummyShader,
@@ -80,24 +82,169 @@ def is_shader_getter(c: Callable) -> bool:
     return True
 
 
-@pytest.fixture(autouse=True)
-def monkeypatch_all_shaders(monkeypatch):
-    """Monkeypatch shader classes and the GL context with mocks.
+@pytest.fixture
+def mock_batch_factory() -> Callable:
+    """A callable which returns Batch mocks for tests.
 
-    The dummy shader classes it substitutes automatically parse shader
-    source to create mock vertex list objects populated with appropriate
-    attributes.
+    The default value is a callable Mock to provide developers
+    with access to the call history during debugging.
+    """
+    return Mock(side_effect=lambda : NonCallableMagicMock(spec=Batch))
+
+
+@pytest.fixture
+def mock_group_factory() -> Callable:
+    """A callable which returns Group mocks for tests.
+
+    The default value is a callable Mock to provide developers
+    with access to the call history during debugging.
+    """
+    return Mock(side_effect=lambda : NonCallableMagicMock(spec=Group))
+
+
+@pytest.fixture
+def default_context_object_space():
+    """A garbage collection record keeper of "doomed" objects.
+
+    It appears safe to first mock, then ignore for any non-drawing
+    (i.e. unit) tests.
+    """
+    return NonCallableMagicMock(spec=ObjectSpace)
+
+
+@pytest.fixture
+def pyglet_graphics_default_group(mock_group_factory):
+    """A mock to assign to the corresponding name on the context"""
+    return mock_group_factory()
+
+
+@pytest.fixture
+def pyglet_graphics_default_batch(mock_batch_factory):
+    """A mock to assign to the corresponding name on the context"""
+    return mock_batch_factory()
+
+
+@pytest.fixture
+def default_gl_context(
+        pyglet_graphics_default_group,
+        pyglet_graphics_default_batch,
+        default_context_object_space
+):
+    """A per-test GL context with the attribute names above set.
+
+    .. warning:: Never use ``spec_set`` to build Context mocks!
+
+                 It will cause all tests for modules with on-demand
+                 loading of default shaders to fail. The current
+                 design of pyglet sets and reads attributes after
+                 Context object creation, which is the behavior
+                 ``spec_set`` exists to forbid.
+
+    If you need more detail than this fixture's default arguments
+    or behavior can provide, override them per the docstring of
+    :py:func:`.autopatch_shaders`.
+
+    Args:
+         pyglet_graphics_default_group:
+            A mock to use as the returned context's default group.
+         pyglet_graphics_default_batch:
+            A mock to use as the returned context's default batch.
+
+    Returns:
+        A Mock of gl.Context with the passed values as its defaults
+    """
+    ctx = NonCallableMagicMock(spec=gl.Context)
+    ctx.attach_mock(pyglet_graphics_default_group, 'pyglet_graphics_default_group')
+    ctx.attach_mock(pyglet_graphics_default_batch, 'pyglet_graphics_default_batch')
+    # TODO: did this work without the following present? Can it be made to?
+    # shouldn't mock speccing implicitly take care of it?
+    ctx.attach_mock(default_context_object_space, 'object_space')
+
+    return ctx
+
+
+# TODO: verify this should be set to autouse
+@pytest.fixture(autouse=True)
+def gl_automock_config():
+    return {}  # Uses default settings
+
+
+@pytest.fixture(autouse=True)
+def autopatch_shaders(
+        monkeypatch,
+        gl_automock_config,
+        mock_batch_factory,
+        default_gl_context
+):
+    """Replace the shader classes with auto-configuring mock types.
+
+    .. warning:: The default settings make multiple assumptions of tests!
+
+    .. _overriding:: https://docs.pytest.org/en/7.4.x/how-to/fixtures.html#overriding-fixtures-on-various-levels
+
+    Use `pytest's documentation on overriding fixtures <overriding_>`_
+    to help you any of the assumptions below create problems for you:
+
+    .. list-table::
+        :header-rows: 1
+
+        * - Assumption
+          - Way(s) to Address
+
+        * - The default values for :py:func:`.default_gl_context`
+            are acceptable.
+
+          - Override the fixtures below:
+
+            * :py:func:`pyglet_graphics_default_group`
+            * :py:func:`pyglet_graphics_default_batch`
+            * :py:func:`default_context_object_space`
+
+            If you need even greater control, you can override
+            :py:func:`default_gl_context` entirely.
+
+        * - Only one simulated window and context are needed.
+          - Build custom mock fixtures.
+
+        * - Tests only need the vertex attributes of shaders.
+          - 1. Extend the partial shader parsing implementation in
+               :py:module:`tests.base.shader_parse_helpers`
+            2. Ask for help doing so
+            3. Use the fallback shader patching tools.
+
+
+    TODO: review this design further. This might be brittle and bad.
+
+    ============ ========= =====================================
+    Reserved key Default   Action
+    ============ ========= =====================================
+    ``ctx``      ``True``  Patch the GL context
+    ``shaders``  ``True``  Patch the shader classes if ``True``.
+
 
     Args:
         monkeypatch: pytest's built-in monkeypatch fixture.
+        gl_autospec_config: A dict of config options.
     """
+    config: Dict[str, Union[bool, str]] = dict(
+        ctx=default_gl_context,
+        shaders=True)
 
-    def _patch_shader_part(name, class_):
-        monkeypatch.setattr(f"pyglet.graphics.shader.{name}", class_)
+    # Can be empty dict or None
+    if gl_automock_config:
+        config.update(gl_automock_config)
 
-    _patch_shader_part("Shader", DummyShader)
-    _patch_shader_part("ShaderProgram", DummyShaderProgram)
-    monkeypatch.setattr("pyglet.gl.current_context", NonCallableMagicMock(gl.Context))
+    if config_ctx := config['ctx']:
+        monkeypatch.setattr("pyglet.gl.current_context", config_ctx)
+
+    monkeypatch.setattr('pyglet.graphics.Batch', mock_batch_factory)
+
+    if config['shaders']:
+        def _patch_shader_part(name, class_):
+            monkeypatch.setattr(f"pyglet.graphics.shader.{name}", class_)
+
+        _patch_shader_part("Shader", DummyShader)
+        _patch_shader_part("ShaderProgram", DummyShaderProgram)
 
 
 
